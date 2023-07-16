072ce12411facbb29d832a39af2aec07bfe355f6
Theader <thiago.huebr@hotmail.com>
2023-07-16 18:54:52 -0300

    Ajuste adicionada dependência do projeto web ao projeto Ioc

--- CursoOnline.DAL/Contextos/ApplicationDbContext.cs
+++ CursoOnline.DAL/Contextos/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using CursoOnline.Dominio.Cursos;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using System.Threading.Tasks;
 
 namespace CursoOnline.DAL.Contextos
@@ -7,7 +8,7 @@ namespace CursoOnline.DAL.Contextos
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Curso> Cursos { get; set; }
-
+        public ApplicationDbContext() { }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- CursoOnline.Web/Startup.cs
+++ CursoOnline.Web/Startup.cs
@@ -1,3 +1,4 @@
+using CursoOnline.Ioc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -22,6 +23,8 @@ namespace CursoOnline.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            StartupIoc.ConfigureServices(services, Configuration);
+            services.AddMvc();
             services.AddControllersWithViews();
         }
 
